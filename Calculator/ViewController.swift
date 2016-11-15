92a83af0f974b800f991db53df2c4499e9b70f56
Pham Ngoc Hai <PhamNgocHai@Macbooks-MacBook-Pro.local>
2016-11-15 15:41:21 +0700

    first commit

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var distanceBtTxFandLbl: NSLayoutConstraint!
+    @IBOutlet weak var distanceBtLblAndMiddlerView: NSLayoutConstraint!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func updateViewConstraints() {
+        super.updateViewConstraints()
+        updateConstraints()
     }
-
+    
+    
+    func updateConstraints()
+     -> Void {
+        let screen = UIScreen.main.bounds.size.height / 667
+        distanceBtLblAndMiddlerView.constant = UIScreen.main.bounds.size.height > 480 ?
+            distanceBtLblAndMiddlerView.constant * screen : distanceBtLblAndMiddlerView.constant * 0.1
+        distanceBtTxFandLbl.constant = UIScreen.main.bounds.size.height > 480 ?
+            distanceBtTxFandLbl.constant * screen : distanceBtTxFandLbl.constant * 0.1
+        
+    }
 
 }
 
---
+++ Readme.md
@@ -0,0 +1,2 @@
+Calculator written by Pham Ngoc Hai
+TechMaster
